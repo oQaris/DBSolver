8856d50460bf74bc546181546e97e6bf509e694f
Sergey Romanov <romanovomsk2014@gmail.com>
2022-02-01 21:30:20 +0600

    Обновлены версии зависимостей

--- app/src/main/java/com/pryanik/dbsolver/logic/algorithms/DcmpTypes.kt
+++ app/src/main/java/com/pryanik/dbsolver/logic/algorithms/DcmpTypes.kt
@@ -1,9 +1,11 @@
 package com.pryanik.dbsolver.logic.algorithms
 
+import com.pryanik.dbsolver.logic.FuncDeps
+
 data class Attr(val lit: String, val type: TypeAttr)
 
 enum class TypeAttr(val str: String) {
-    PRIMARY_KEY("PK"), FOREIGN_KEY("FK"), NOT_KEY("")
+    PRIMARY_KEY("(PK)"), FOREIGN_KEY("(FK)"), NOT_KEY("")
 }
 
 class Relation(private val allAttr: MutableSet<Attr> = mutableSetOf()) :
@@ -21,9 +23,21 @@ class Relation(private val allAttr: MutableSet<Attr> = mutableSetOf()) :
 
     fun toSetLit() = this.map { it.lit }
 
-    override fun toString() = "(${this.joinToString(",") { "${it.lit} (${it.type.str})" }})"
+    override fun toString() = "(${this.joinToString(",") { "${it.lit}${it.type.str}" }})"
 
     fun copy() = Relation(allAttr.map { it.copy() }.toMutableSet())
+
+    override fun equals(other: Any?): Boolean {
+        if (other === this)
+            return true
+        if (other !is Relation)
+            return false
+        return allAttr == other.allAttr
+    }
+
+    override fun hashCode(): Int {
+        return allAttr.hashCode()
+    }
 }
 
 class Decomposition(val dcmpSet: MutableSet<Relation> = mutableSetOf()) :
@@ -34,6 +48,20 @@ class Decomposition(val dcmpSet: MutableSet<Relation> = mutableSetOf()) :
     }
 
     fun copy() = Decomposition(dcmpSet.map { it.copy() }.toMutableSet())
+
+    override fun toString() = this.joinToString("\n")
+
+    override fun equals(other: Any?): Boolean {
+        if (other === this)
+            return true
+        if (other !is Decomposition)
+            return false
+        return dcmpSet == other.dcmpSet
+    }
+
+    override fun hashCode(): Int {
+        return dcmpSet.hashCode()
+    }
 }
 
 // Extensions
@@ -43,3 +71,18 @@ fun Iterable<String>.toRelation() = Relation(this.map { it.toAttr() }.toMutableS
 fun String.toAttr(type: TypeAttr = TypeAttr.NOT_KEY) = Attr(this, type)
 
 fun Iterable<Relation>.toDecomposition() = Decomposition(this.toMutableSet())
+
+fun FuncDeps.extractFor(rel: Relation): FuncDeps {
+    val setLitInRel = rel.toSetLit()
+    val newFD = FuncDeps()
+    this.forEach { (det, dep) ->
+        if (setLitInRel.containsAll(det)) {
+            val newDep = dep intersect setLitInRel
+            if (newDep.isNotEmpty())
+                newFD[det] = newDep.toMutableSet()
+        }
+    }
+    return newFD
+}
+
+fun FuncDeps.toUniversalDcmp() = listOf(this.allAttr.toRelation()).toDecomposition()
